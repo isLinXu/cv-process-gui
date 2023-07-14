e3a5854bcb3013ac21bfefeba7fb6cea891ad444
isLinXu <2267379130@qq.com>
2023-07-15 03:46:26 +0800

    update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import filedialog, messagebox
 from PIL import Image, ImageTk
 from cvlibs.process import CVProcess  # 导入CVProcess类
 
+
 class ImageProcessingGUI:
     def __init__(self, master):
         self.master = master
@@ -25,79 +26,81 @@ class ImageProcessingGUI:
         self.save_button = tk.Button(self.top_frame, text="Save Image", command=self.save_image, width=10, height=1)
         self.save_button.pack(side=tk.RIGHT, padx=5)
 
-
         # 第一列按钮
         self.gray_button = tk.Button(self.button_frame, text="Grayscale", command=self.convert_grayscale, width=10,height=1)
         self.gray_button.grid(row=0, column=0, pady=5)
 
-        self.edge_button = tk.Button(self.button_frame, text="Edge Detection", command=self.edge_detection, width=10,height=1)
+        self.edge_button = tk.Button(self.button_frame, text="Edge Detection", command=self.edge_detection, width=10, height=1)
         self.edge_button.grid(row=1, column=0, pady=5)
 
         self.rotate_button = tk.Button(self.button_frame, text="Rotate", command=self.rotate_image, width=10, height=1)
         self.rotate_button.grid(row=2, column=0, pady=5)
 
-        # 第二列按钮
         self.flip_button = tk.Button(self.button_frame, text="Flip", command=self.flip_image, width=10, height=1)
-        self.flip_button.grid(row=0, column=1, pady=5)
+        self.flip_button.grid(row=3, column=0, pady=5)
 
         self.scale_button = tk.Button(self.button_frame, text="Scale", command=self.scale_image, width=10, height=1)
-        self.scale_button.grid(row=1, column=1, pady=5)
+        self.scale_button.grid(row=4, column=0, pady=5)
 
-        self.hist_eq_button = tk.Button(self.button_frame, text="Histogram Equalization", command=self.hist_eq_image,
-                                        width=10, height=1)
-        self.hist_eq_button.grid(row=2, column=1, pady=5)
+        self.hist_eq_button = tk.Button(self.button_frame, text="Histogram Equalization", command=self.hist_eq_image,width=15, height=1)
+        self.hist_eq_button.grid(row=5, column=0, pady=5)
 
-        # 第三列按钮
-        self.morph_button = tk.Button(self.button_frame, text="Morphology", command=self.morph_image,width=10, height=1)
-        self.morph_button.grid(row=0, column=2, pady=5)
+        self.morph_button = tk.Button(self.button_frame, text="Morphology", command=self.morph_image, width=10, height=1)
+        self.morph_button.grid(row=6, column=0, pady=5)
 
         self.blur_button = tk.Button(self.button_frame, text="Blur", command=self.blur_image, width=10, height=1)
-        self.blur_button.grid(row=1, column=2, pady=5)
+        self.blur_button.grid(row=7, column=0, pady=5)
 
-        self.sharpen_button = tk.Button(self.button_frame, text="Sharpen", command=self.sharpen_image, width=10,height=1)
-        self.sharpen_button.grid(row=2, column=2, pady=5)
+        self.sharpen_button = tk.Button(self.button_frame, text="Sharpen", command=self.sharpen_image, width=10, height=1)
+        self.sharpen_button.grid(row=8, column=0, pady=5)
+
+        self.noise_button = tk.Button(self.button_frame, text="noise reduction", command=self.noise_reduction, width=10,height=1)
+        self.noise_button.grid(row=9, column=0, pady=5)
+
+        self.contour_detection_button = tk.Button(self.button_frame, text="contour_detection", command=self.contour_detection, width=10,height=1)
+        self.contour_detection_button.grid(row=10, column=0, pady=5)
+
+        self.color_space_conversion_button = tk.Button(self.button_frame, text="color_space_conversion",command=self.color_space_conversion, width=15,height=1)
+        self.color_space_conversion_button.grid(row=11, column=0, pady=5)
+
+        self.adaptive_threshold_button = tk.Button(self.button_frame, text="Adaptive Threshold",command=self.adaptive_threshold, width=10, height=1)
+        self.adaptive_threshold_button.grid(row=12, column=0, pady=5)
+
+        self.canvas = tk.Canvas(master, width=1080, height=960)
+        self.canvas.pack(side=tk.RIGHT)
+
 
         # 底部输入框和滑动条
-        self.brightness_label = tk.Label(self.button_frame, text="Brightness")
-        self.brightness_label.grid(row=3, column=0)
-        self.brightness_slider = tk.Scale(self.button_frame, from_=-100, to=100, orient=tk.HORIZONTAL, length=150)
-        self.brightness_slider.set(0)
-        self.brightness_slider.grid(row=3, column=1)
-        self.brightness_button = tk.Button(self.button_frame, text="Apply Brightness", command=self.adjust_brightness,
+        self.common_label = tk.Label(self.master, text="input number:")
+        self.common_label.place(relx=0.2, rely=0.83)
+
+        self.common_entry = tk.Entry(self.master)
+        self.common_entry.place(relx=0.3, rely=0.83)
+
+        self.common_label = tk.Button(self.master, text="Apply Scale", command=self.custom_scale_image, width=10,height=1)
+        self.common_label.place(relx=0.7, rely=0.83)
+
+        self.brightness_label = tk.Label(self.master, text="slider:")
+        self.brightness_label.place(relx=0.2, rely=0.9)
+
+        self.common_slider = tk.Scale(self.master, from_=-255, to=255, orient=tk.HORIZONTAL, length=400)
+        self.common_slider.set(0)
+        self.common_slider.place(relx=0.3, rely=0.9)
+
+        self.brightness_button = tk.Button(self.master, text="Apply Brightness", command=self.adjust_brightness,
                                            width=10, height=1)
-        self.brightness_button.grid(row=3, column=2, pady=5)
+        self.brightness_button.place(relx=0.7, rely=0.88)
 
-        self.threshold_label = tk.Label(self.button_frame, text="Threshold")
-        self.threshold_label.grid(row=4, column=0)
-        self.threshold_slider = tk.Scale(self.button_frame, from_=0, to=255, orient=tk.HORIZONTAL, length=150)
-        self.threshold_slider.grid(row=4, column=1)
-        self.threshold_button = tk.Button(self.button_frame, text="Apply Threshold", command=self.apply_threshold,
-                                          width=10, height=1)
-        self.threshold_button.grid(row=4, column=2, pady=5)
+        self.threshold_label = tk.Label(self.master, text="Threshold")
+        self.threshold_button = tk.Button(self.master, text="Apply Threshold", command=self.apply_threshold, width=10,height=1)
+        self.threshold_button.place(relx=0.7, rely=0.93)
 
-        # 在底部添加自定义缩放输入框和按钮
-        self.scale_label = tk.Label(self.button_frame, text="Scale Ratio")
-        self.scale_label.grid(row=7, column=0)
-        self.scale_entry = tk.Entry(self.button_frame)
-        self.scale_entry.grid(row=7, column=1)
-        self.scale_button = tk.Button(self.button_frame, text="Custom Scale", command=self.custom_scale_image, width=15,
-                                      height=2)
-        self.scale_button.grid(row=7, column=2, pady=5)
-
-        # 在底部添加自适应阈值按钮
-        self.adaptive_threshold_button = tk.Button(self.button_frame, text="Adaptive Threshold",
-                                                   command=self.adaptive_threshold, width=15, height=2)
-        self.adaptive_threshold_button.grid(row=8, column=0, pady=5)
-
-
-        self.canvas = tk.Canvas(master, width=800, height=600)
-        self.canvas.pack(side=tk.RIGHT)
 
 
     def open_image(self):
         file_path = filedialog.askopenfilename()
         if file_path:
-            self.original_image = self.cv_process.open_image(file_path)  # 使用CVProcess类的实例打开图像
+            self.original_image = self.cv_process.open_image(file_path)
             self.processed_image = self.original_image.copy()
             self.show_images()
 
@@ -109,19 +112,17 @@ class ImageProcessingGUI:
 
     def show_images(self):
         if self.original_image and self.processed_image:
-            original_image = self.cv_process.resize_image(self.original_image, 400, 300)
-            processed_image = self.cv_process.resize_image(self.processed_image, 400, 300)
+            original_image = self.cv_process.resize_image(self.original_image, 540, 480)
+            processed_image = self.cv_process.resize_image(self.processed_image, 540, 480)
 
             original_photo = ImageTk.PhotoImage(original_image)
             processed_photo = ImageTk.PhotoImage(processed_image)
 
             self.canvas.create_image(0, 0, anchor=tk.NW, image=original_photo)
-            self.canvas.create_image(400, 0, anchor=tk.NW, image=processed_photo)
-
+            self.canvas.create_image(540, 0, anchor=tk.NW, image=processed_photo)
             self.canvas.image1 = original_photo
             self.canvas.image2 = processed_photo
 
-    # 在处理函数中，使用CVProcess类的实例调用图像处理方法
     def convert_grayscale(self):
         if self.original_image:
             self.processed_image = self.cv_process.convert_grayscale(self.original_image)
@@ -144,7 +145,7 @@ class ImageProcessingGUI:
 
     def adjust_brightness(self):
         if self.original_image:
-            brightness = self.brightness_slider.get()
+            brightness = self.common_slider.get()
             self.processed_image = self.cv_process.adjust_brightness(self.original_image, brightness)
             self.show_images()
 
@@ -175,18 +176,18 @@ class ImageProcessingGUI:
 
     def apply_threshold(self):
         if self.original_image:
-            threshold = self.threshold_slider.get()
+            # threshold = self.threshold_slider.get()
+            threshold = self.common_slider.get()
             self.processed_image = self.cv_process.apply_threshold(self.original_image, threshold)
             self.show_images()
 
     def custom_scale_image(self):
         if self.original_image:
             try:
-                scale_ratio = float(self.scale_entry.get())
+                scale_ratio = float(self.common_entry.get())
                 self.processed_image = self.cv_process.custom_scale_image(self.original_image, scale_ratio)
                 self.show_images()
             except ValueError:
-                # 如果输入的缩放比例无效，显示一个错误消息
                 messagebox.showerror("Error", "Invalid scale ratio. Please enter a valid number.")
 
     def adaptive_threshold(self):
