cc14c7d73a118ac2f6bed13de9194ee7ad6f8045
isLinXu <2267379130@qq.com>
2023-07-15 21:01:13 +0800

    [cvlibs] update process_fun.py

--- cvlibs/process_fun.py
+++ cvlibs/process_fun.py
@@ -1,6 +1,9 @@
 
 import cv2
 import numpy as np
+import PySimpleGUI as sg
+from matplotlib import pyplot as plt
+
 
 def open_image(file_path):
     img = cv2.imread(file_path)
@@ -113,3 +116,74 @@ def hough_circles(img):
             cv2.circle(img_circles, (i[0], i[1]), 2, (0, 0, 255), 3)
         return img_circles
 
+def blend_images(img1, img2, alpha):
+    return cv2.addWeighted(img1, alpha, img2, 1-alpha, 0)
+
+def stitch_images(img1, img2):
+    stitcher = cv2.Stitcher.create(cv2.Stitcher_PANORAMA)
+    status, stitched_img = stitcher.stitch((img1, img2))
+    if status == 0:
+        return stitched_img
+    else:
+        sg.popup("图像缝合失败")
+    return img1
+
+def average_blur(img, ksize):
+    return cv2.blur(img, (ksize, ksize))
+
+def bilateral_filter(img, d, sigma_color, sigma_space):
+    return cv2.bilateralFilter(img, d, sigma_color, sigma_space)
+
+
+def warp_affine_transform(img, M):
+    rows, cols = img.shape[:2]
+    return cv2.warpAffine(img, M, (cols, rows))
+
+def compute_histogram(img):
+    if len(img.shape) == 3:
+        color = ('b', 'g', 'r')
+        for i, col in enumerate(color):
+            histr = cv2.calcHist([img], [i], None, [256], [0, 256])
+            plt.plot(histr, color=col)
+            plt.xlim([0, 256])
+    else:
+        histr = cv2.calcHist([img], [0], None, [256], [0, 256])
+        plt.plot(histr)
+        plt.xlim([0, 256])
+        plt.show()
+
+def global_threshold(img, thresh_value):
+    _, thresh_img = cv2.threshold(img, thresh_value, 255, cv2.THRESH_BINARY)
+    return thresh_img
+
+def adaptive_threshold(img):
+    return cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 8, 2)
+
+def split_channels(img):
+    return cv2.split(img)
+
+def merge_channels(channels):
+    return cv2.merge(channels)
+
+def convert_color_space(img, code):
+    return cv2.cvtColor(img, code)
+
+def detect_shapes(img):
+    img_gray = grayscale(img)
+    img_canny = canny_edge_detection(img_gray, 100, 200)
+    contours, _ = cv2.findContours(img_canny, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    img_shapes = img.copy()
+    for cnt in contours:
+        # 矩形
+        x, y, w, h = cv2.boundingRect(cnt)
+        cv2.rectangle(img_shapes, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        # 圆形
+        (x, y), radius = cv2.minEnclosingCircle(cnt)
+        center = (int(x), int(y))
+        radius = int(radius)
+        cv2.circle(img_shapes, center, radius, (255, 0, 0), 2)
+        # 椭圆形
+        if len(cnt) >= 5:
+            ellipse = cv2.fitEllipse(cnt)
+            cv2.ellipse(img_shapes, ellipse, (0, 0, 255), 2)
+    return img_shapes
